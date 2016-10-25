c1ba4449b6ed0c83f909e9239033ec40997dc12c
DARRYL LONG <dlong3@sae.au>
2016-10-25 10:39:27 +1000

    Initial Character Design with stat conversion

--- MDU112Assignment2/MDU112Assignment2/Character.cs
+++ MDU112Assignment2/MDU112Assignment2/Character.cs
@@ -8,16 +8,43 @@ namespace MDU112Assignment2
 {
     public class Character
     {
-        //Declare character variables
+        private int Agility;
+        private int Stamina;
+        private int Strength;
+        private int Acuity;
 
         public Character()
         {
-            //Initialise variables with base values
+            Random rand = new Random();
 
+            Agility = rand.Next(5, 16);
+            Stamina = rand.Next(5, 16);
+            Strength = rand.Next(5, 16);
+            Acuity = rand.Next(5, 16);
 
             //Call function that allocates random attributes
         }
 
+        //Functions to retrieve stats
+        private int Health
+        {
+            get { return Stamina * 10; }
+        }
+
+        private int Speed
+        {
+            get { return Agility * 2; }
+        }
+
+        private double Damage
+        {
+            get { return Strength * 1.5; }
+        }
+
+        private double CritChance
+        {
+            get { return Acuity * 0.01; }
+        }
         //Function to get stats, automatically converting attributes to stats
 
         //Allow game to get attributes? only stats?
